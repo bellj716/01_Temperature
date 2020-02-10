869aef456f72cf3499fce552cb64baaee4f64f88
bellj716 <51768193+bellj716@users.noreply.github.com>
2020-02-11 10:45:02 +1300

    PAPYRUS

--- temp_convert/00_tkinter_template.py
+++ temp_convert/00_tkinter_template.py
@@ -11,8 +11,21 @@ if __name__ == '__main__':
             background_color = "light blue"
 
             # Converter Main Screen GUI
-            self.converter_frame = Frame(width=500, height=700, bg=background_color)
+            self.converter_frame = Frame(width=500, height=700, bg=background_color, pady=10)
             self.converter_frame.grid()
+
+            # Temperature converion heading row 0
+            self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+                                              font=("Papyrus", "18", "bold"),
+                                              bg=background_color,
+                                              padx=10, pady=10)
+            self.temp_converter_label.grid(row=0)
+
+            #help button (row 1)
+            self.help_button = Button(self.converter_frame, text="help",
+                                      font=("Papyrus", "12"),
+                                      padx=10, pady=-10)
+            self.help_button.grid(row=1)
 
 # main routine
 if __name__ == "__main__":
