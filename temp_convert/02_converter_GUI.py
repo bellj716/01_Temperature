96f4faeedb51bd777d82198791761cd8c2d548e0
bellj716 <51768193+bellj716@users.noreply.github.com>
2020-02-14 13:15:17 +1300

    s

--- temp_convert/02_converter_GUI.py
+++ temp_convert/02_converter_GUI.py
@@ -2,7 +2,6 @@ from tkinter import *
 from functools import partial       # to prevent unwanted windows
 import random
 
-
 class Converter:
     def __init__(self, parent):
 
@@ -14,34 +13,49 @@ class Converter:
         self.converter_frame.grid()
 
         # Temperature Converter Heading
-        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+        self.temp_heading_label = Label(self.converter_frame, text="Temperature Converter",
                                           font=("Arial", "18", "bold"),
                                           bg=background_color,
                                           padx=10, pady=10)
-        self.temp_converter_label.grid(row=0)
+        self.temp_heading_label.grid(row=0)
 
         # User instructions (row 1)
-            self.help_button = Button(self.converter_frame, text="help",
-                                      font=("Arial", "14"),
-                                      padx=10, pady=-10, command=self.help)
-            self.help_button.grid(row=1)
-
-        def help(self):
-            print("YOU HAVE REQUESTED HELP")
-            get_help = Help(self)
-            get_help.help_text.configure(text="HELP TEXT GOES HERE", font="Arial 8")
+        self.temp_instructions_label = Label(self.converter_frame,
+                                                 text="Type in the amount to be "
+                                                      "converted and then push "
+                                                      "one of the buttons below",
+                                                 font=("Arial", "14"), wrap=250, justify=LEFT,
+                                                 bg=background_color, padx=10, pady=-10)
+        self.temp_instructions_label.grid(row=1)
 
         # Temperature Entry Box (row 2)
+        self.to_convert_entry = Entry(self.converter_frame, width=20,
+                                      font="Arial 14")
+        self.to_convert_entry.grid(row=2)
 
-        # Conversion buttons frame (row 3)
+        # Conversion buttons frame (row 3), orchid3 | khaki1
+        self.conversion_buttons_frame = Frame(self.converter_frame)
+        self.conversion_buttons_frame.grid(row=3, pady=10)
+
+        self.to_c_button = Button(self.conversion_buttons_frame,
+                                  text="To Celsius", font="Arial 12 bold",
+                                  bg="khaki1", padx=10, pady=-10)
+        self.to_c_button.grid(row=0, column=0)
+
+        self.to_f_button = Button(self.conversion_buttons_frame,
+                                  text="To Fahrenheit", font="Arial 12 bold",
+                                  bg="Orchid1", padx=10, pady=-10)
+        self.to_f_button.grid(row=0, column=1)
 
         # Answer Label (row 4)
+
 
         # History / Help button frame (row 5)
 
 # main routine
 if __name__ == "__main__":
     root = Tk()
-    root.title("title goes here")
-    something = Foo(root)
+    root.title("Temperature Convertwe")
+    root.resizable(width=False, height=False)
+    something = Converter(root)
     root.mainloop()
