ffcf558ac2394379cab62f706b3b3bc0dd2c58b9
bellj716 <51768193+bellj716@users.noreply.github.com>
2020-02-18 10:50:49 +1300

    1234567

--- temp_convert/02_converter_GUI.py
+++ temp_convert/02_converter_GUI.py
@@ -54,6 +54,16 @@ class Converter:
         self.converted_label.grid(row=4)
 
         # History / Help button frame (row 5)
+        self.hist_help_frame = Frame(self.converter_frame)
+        self.hist_help_frame.grid(row=5, pady=10)
+
+        self.calc_hist_button = Button(self.hist_help_frame, font="Arial 14 bold",
+                                       text="Calculation History", width=15)
+        self.calc_hist_button.grid(row=0, column=0)
+
+        self.help_button = Button(self.hist_help_frame, font="Arial 14 bold",
+                                  text="Help", width=5)
+        self.help_button.grid(row=0, column=1)
 
 # main routine
 if __name__ == "__main__":
