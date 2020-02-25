24b49b4fa9884d7baed9d121fd7500e4c410a664
bellj716 <51768193+bellj716@users.noreply.github.com>
2020-02-26 09:55:08 +1300

    07_most_recent finished

--- temp_convert/07_most_recent.py
+++ temp_convert/07_most_recent.py
@@ -4,18 +4,35 @@
 all_calculations = []
 
 # get five items of data
-for item in range(0, 5):
+get_item = ""
+while get_item != "xxx":
     get_item = input("Enter an item: ")
+
+    if get_item == "xxx":
+        break
+
     all_calculations.append(get_item)
 
-# show that everything made it to the list...
-print()
-print("*** The Full List ***")
-print(all_calculations)
-
 print()
 
-print("*** Most Recent 3 ***")
-# print items starting at the END of the list
-for item in range(0,3):
-    print(all_calculations[len(all_calculations) - item - 1])
+if len(all_calculations) == 0:
+    print("Oops - the list is empty")
+
+else:
+
+    # show that everything made it to the list...
+    print()
+    print("*** The Full List ***")
+    print(all_calculations)
+
+    print()
+
+    if len(all_calculations) >= 3:
+        print("*** Most Recent 3 ***")
+        # print items starting at the END of the list
+        for item in range(0,3):
+            print(all_calculations[len(all_calculations) - item - 1])
+    else:
+        print("*** Items from Newest to Oldest ***")
+        for item in all_calculations:
+            print(all_calculations[len(all_calculations) - all_calculations.index(item-1)])
