36a3ead366087e52a388008ab65b87cf2416a169
bellj716 <51768193+bellj716@users.noreply.github.com>
2020-02-28 13:12:24 +1300

    11

--- temp_convert/08_Data_to_txtV1.py
+++ temp_convert/08_Data_to_txtV1.py
@@ -1,4 +1,21 @@
 # Source: https://www.guru99.com/reading-and-writing-files-in-python.html
 
 # Data to be outputted
-data = ['First', 'second', 'third']
+data = ['First', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh']
+
+# get filename, cant be blank / invalid
+# assume valid data for now
+filename = input("Enter a Filename (leave off the extension): ")
+
+# add .txt suffix!
+filename += ".txt"
+
+# create file to hold data
+f = open(filename, "w+")
+
+# add new line at the end of each item
+for item in data:
+    f.write(item + "\n")
+
+# colse file
+f.close()
